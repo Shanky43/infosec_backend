aaedd1dcf9851648de72b96ae5cf356502886ca7
Shankar S <112791993+Shanky43@users.noreply.github.com>
2023-07-02 00:15:08 +0530

    added code

--- Model/Infosec.model.js
+++ Model/Infosec.model.js
@@ -1,9 +1,20 @@
 const { Schema, model } = require("mongoose")
 
 const infosecSchema = Schema({
-
-    likelihood: { type: Number },
-
+    fullName: { type: String },
+    designation: { type: String },
+    emailaddress: { type: String },
+    dob: { type: String },
+    contactnumber: { type: String },
+    city: { type: String },
+    profilepicture: { type: String },
+    linkedin: { type: String },
+    twitter: { type: String },
+    facebook: { type: String },
+    instagram: { type: String },
+    other: { type: String },
+    // companydescription: { type: String },
+    // companyWebsite: { type: String }
 }, {
     versionKey: false
 })
--- Routes/infosec.route.js
+++ Routes/infosec.route.js
@@ -3,9 +3,19 @@ const express = require("express")
 const { infosecModel } = require("../Model/Infosec.model");
 const infosecDataRouter = express.Router()
 
-infosecDataRouter.get("/", async (req, res) => {
+infosecDataRouter.post("/userdata", async (req, res) => {
+    console.log(req.body)
     try {
+        const infosec = infosecModel(req.body);
+        await infosec.save();
+        res.status(200).send("Successfully added Data");
+    } catch (error) {
+        res.status(502).json({ error: error.message })
+    }
+})
 
+infosecDataRouter.get("/userdata", async (req, res) => {
+    try {
         const infosecData = await infosecModel.find(req.query);
         console.log(infosecData)
         res.status(200).json({ infosecData })
